ed417bc43dc0bc6a750cb6db7ee935f8cb96a246
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-03-22 13:38:45 +0530

    project setup and requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="carprice",
+    version="0.0.1",
+    author="Harshal Kumre",
+    author_email="kumreharshalkumar@gmail.com",
+    packages=find_packages()
+)
