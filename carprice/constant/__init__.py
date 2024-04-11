682ef180633e0db0f413ef81f2816f29881eb03c
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-04-11 15:31:36 +0530

    data transformation added

--- carprice/constant/__init__.py
+++ carprice/constant/__init__.py
@@ -13,6 +13,8 @@ COLLECTION_NAME = "car"
 FILE_NAME: str = "car.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+PREROCESSING_OBEJCT_FILE_NAME = "preprocessing.pkl"
+TARGET_COLUMN = "selling_price"
 
 BUCKET_NAME = "carprice-2024"
 
@@ -37,3 +39,10 @@ DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 DATA_VALIDATION_DRIFT_REPORT_PAGE_FILE_NAME: str = "report.html"
+
+"""
+Data Transformation related constants start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transfromation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
--- carprice/utils/main_utils.py
+++ carprice/utils/main_utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 import yaml
 import dill
+
+import numpy as np
 from carprice.exception import CarPriceException
 from carprice.logger import logging
 
@@ -37,5 +39,20 @@ def save_object(file_path: str, obj: object) -> None:
 
         logging.info("Exited the save_object method of utils")
 
+    except Exception as e:
+        raise CarPriceException(e, sys) from e
+    
+    
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
     except Exception as e:
         raise CarPriceException(e, sys) from e
