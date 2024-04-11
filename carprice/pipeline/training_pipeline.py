3fd6bd320dfccee2f8cde5e9a743e1fba284081e
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-04-11 14:19:09 +0530

    data validation added

--- carprice/pipeline/training_pipeline.py
+++ carprice/pipeline/training_pipeline.py
@@ -4,15 +4,19 @@ from carprice.exception import CarPriceException
 from carprice.logger import logging
 
 from carprice.components.data_ingestion import DataIngestion
+from carprice.components.data_validation import DataValidation
 
-from carprice.entity.config_entity import DataIngestionConfig
+from carprice.entity.config_entity import (DataIngestionConfig,
+                                           DataValidationConfig)
 
-from carprice.entity.artifact_entity import DataIngestionArtifact
+from carprice.entity.artifact_entity import (DataIngestionArtifact,
+                                             DataValidationArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
         
     
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -21,14 +25,32 @@ class TrainPipeline:
         """
         try:
             logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-            logging.info("Getting the data from gcloud")
+            logging.info("Getting the data from mongodb")
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train_set and test_set from gcloud")
+            logging.info("Got the train_set and test_set from mongodb")
             logging.info(
                 "Exited the start_data_ingestion method of TrainPipeline class"
             )
             return data_ingestion_artifact
+        except Exception as e:
+            raise CarPriceException(e, sys) from e
+        
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation operation")
+            
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+            return data_validation_artifact
         except Exception as e:
             raise CarPriceException(e, sys) from e
         
@@ -39,5 +61,7 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise CarPriceException(e, sys) from e
--- carprice/utils/main_utils.py
+++ carprice/utils/main_utils.py
@@ -1,6 +1,9 @@
+import os 
 import sys
 import yaml
+import dill
 from carprice.exception import CarPriceException
+from carprice.logger import logging
 
 
 
@@ -8,6 +11,31 @@ def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise CarPriceException(e, sys) from e
+    
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise CarPriceException(e, sys) from e
+    
+def save_object(file_path: str, obj: object) -> None:
+    logging.info("Entered the save_object method of utils")
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+        logging.info("Exited the save_object method of utils")
 
     except Exception as e:
         raise CarPriceException(e, sys) from e
